06b763cdedd5856f9da1a06fa1a0df7afe242bce
Santiago <santiago-perez-perez @hotmail.com>
2017-09-09 18:20:07 -0300

    Grid añadido

---
+++ app/src/main/java/com/example/santi/inriegoapp/EstablecimientoActivity.java
@@ -0,0 +1,20 @@
+package com.example.santi.inriegoapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by santi on 29/8/2017.
+ */
+
+public class EstablecimientoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
