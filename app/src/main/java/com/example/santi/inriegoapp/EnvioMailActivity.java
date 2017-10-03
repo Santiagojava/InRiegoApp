a026e4f3ef50adfc5a2e605d7674591d91806bc6
Santiago <santiago-perez-perez @hotmail.com>
2017-10-02 23:20:08 -0300

    Funcion email

---
+++ app/src/main/java/com/example/santi/inriegoapp/EnvioMailActivity.java
@@ -0,0 +1,93 @@
+package com.example.santi.inriegoapp;
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+/**
+ * Created by santi on 2/10/2017.
+ */
+
+public class EnvioMailActivity extends AppCompatActivity {
+
+    Session session = null;
+    ProgressDialog pdialog = null;
+    Context context = null;
+    String rec, subject, textMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this;
+
+        enviarmail();
+    }
+    public void enviarmail(){
+        rec = "santiago-perez-perez@hotmail.com";
+        subject = "Esto es una prueba";
+        textMessage = "Hola";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        session = Session.getDefaultInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("sanpp96@gmail.com", "");
+            }
+        });
+
+        pdialog = ProgressDialog.show(context, "", "Enviando Mail...", true);
+
+        RetreiveFeedTask task = new RetreiveFeedTask();
+        task.execute();
+    }
+
+    class RetreiveFeedTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try{
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("testfrom354@gmail.com"));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(rec));
+                message.setSubject(subject);
+                message.setContent(textMessage, "text/html; charset=utf-8");
+                Transport.send(message);
+            } catch(MessagingException e) {
+                e.printStackTrace();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), "Mensaje enviado", Toast.LENGTH_LONG).show();
+        }
+    }
+}
